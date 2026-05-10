29456da7ed69e0c68b7b521d41777191ca963b08
Wesley Alves de Oliveira <wao2ster@gmail.com>
2026-05-10 20:44:14 -0300

    Clean deploy to gh-pages

---
+++ _next/static/oB-Z0whQUpb3a7Njxxaxd/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dis//_next/:path+",
+        "destination": "/dis/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
